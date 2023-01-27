b08ec1d8149e1268d7664f107e37cb3d9d3ff4f8
Darya <filinova.darya00@mail.ru>
2023-01-27 09:28:42 +0900

    v-model

---
+++ src/components/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h3>Список постов</h3>
+        <PostItem  
+            v-for="post in posts"
+            :post="post"/>
+    </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem.vue"
+export default {
+    props: {
+        posts: {
+            type: Array,
+            required: true,
+        }
+    },
+
+    components: {
+        PostItem
+    },
+
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'MyButton'
+}
+</script>
+
+<style>
+.btn {
+    align-self: flex-end;
+    padding: 10px 15px;
+    color: teal;
+    border: 1px solid teal;
+}
+</style>
