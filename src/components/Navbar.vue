2bd59964fd7e1141854cbe6cb086c764bc90c6ce
Darya <filinova.darya00@mail.ru>
2023-02-01 07:29:00 +0900

    static-routing

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <div @click="$router.push('/')">Vue 3</div>
+        <div class="navbar__btns">
+            <MyButton @click="$router.push('/posts')">Посты</MyButton>
+            <MyButton @click="$router.push('/about')" style="margin-left: 20px">О сайте</MyButton>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.navbar {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 10px 15px;
+    background-color: lightgray;
+    box-shadow: 2px 2px 4px gray;
+}
+
+.navbar__btns {
+    margin-left: auto;
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Данное приложение было создано
+            в реузльтате обучения на канале Ulbi
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+        data: () => {
+            return{}
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
